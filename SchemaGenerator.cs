2311b8a3435e1cd75a2c5443893d42cb17cd2b98
Dia <dianaemadale@gmail.com>
2022-10-23 11:00:43 +0100

    add constraint on generic method

--- SchemaGenerator.cs
+++ SchemaGenerator.cs
@@ -13,7 +13,8 @@ namespace SchemaApp
         private static readonly JSchemaGenerator jSchemaGenerator = new();
         private static readonly string _uri = "https://json-schema.org/draft/2019-09/schema";
 
-        public static JSchema Generate<T>(string description)        {            
+        public static JSchema Generate<T>(string description) where T : class, new()
+        {
 
             jSchemaGenerator.GenerationProviders.Add(new StringEnumGenerationProvider());
             var schemas = jSchemaGenerator.Generate(typeof(T));
