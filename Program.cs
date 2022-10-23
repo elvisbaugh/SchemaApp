1a31118f94b3a1a596670223639ce8ecd26455c9
Dia <dianaemadale@gmail.com>
2022-10-23 10:22:45 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Newtonsoft.Json;
+using SchemaApp;
+
+var data = SchemaGenerator.Generate<Person>("New Contract");
+Console.WriteLine(data);
+
+public class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public DateTime Created { get; set; }
+
+}
---
+++ SchemaGenerator.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Schema.Generation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchemaApp
+{
+    public class SchemaGenerator
+    {
+        private static readonly JSchemaGenerator jSchemaGenerator = new();
+        private static readonly string _uri = "https://json-schema.org/draft/2019-09/schema";
+
+        public static JSchema Generate<T>(string description)        {            
+
+            jSchemaGenerator.GenerationProviders.Add(new StringEnumGenerationProvider());
+            var schemas = jSchemaGenerator.Generate(typeof(T));
+
+            schemas.SchemaVersion = new Uri(_uri);
+            schemas.Description = description;
+
+            return schemas;
+        }
+    }
+
+}
